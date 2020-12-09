ed876c482577575bfad5cd201e6d684fe5a8b577
Nalini0888 <75728169+Nalini0888@users.noreply.github.com>
2020-12-09 15:54:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    document.getElementById("heading1").style.color="yellow";
+}
+
+function setDate(){
+    document.getElementById('date').innerHTML = new Date().toDateString();
+}
